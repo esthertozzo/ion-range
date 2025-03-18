0de330106cb3ae240a3b4df833e9b011408cfe36
esthertozzo <tozzo.est@gmail.com>
2025-03-18 12:00:42 -0300

    iniciar projeto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,33 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  valorRange = 0;
+  gorjeta = 0;
+  valorConta = 0;
+  valorTotalConta = 0;
+  convertido = 0;
+
+  tipoMedida = "";
+  quilometros = 0;
+
+  // onIonChange(ev:RangeCustomEvent){
+  //   this.valorRange = parseInt(ev.detail.value.toString());
+  // }
+
+  calcularGorjeta(ev:RangeCustomEvent){
+    this.valorRange = Number(ev.detail.value.toString())/100;
+    this.gorjeta = Math.round(Number(this.valorRange*this.valorConta));
+    this.valorTotalConta = Number(this.gorjeta + this.valorConta);
+  }
+
+  converterMedidas(ev:RangeCustomEvent){
+    this.valorRange = Number(ev.detail.value.toString());
+
+    if(this.tipoMedida === "quilometros"){
+      this.convertido = Number(this.valorRange)*1000;
+    }else{
+      
+    }
+  }
 
 }
