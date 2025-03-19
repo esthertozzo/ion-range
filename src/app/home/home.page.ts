ecfaf551075562688664ee46ace4746909169200
esthertozzo <tozzo.est@gmail.com>
2025-03-18 22:32:38 -0300

    ex 2 feito

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,34 +9,47 @@ import { RangeCustomEvent } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
   valorRange = 0;
   gorjeta = 0;
   valorConta = 0;
   valorTotalConta = 0;
-  convertido = 0;
+  convertido = "";
 
+  valorRangeUM = 0;
+  valorRangeKm = 0;
+  valorRangeM = 0;
   tipoMedida = "";
+  tipoMedidaConverter = "";
   quilometros = 0;
+  milhas = 0;
 
   // onIonChange(ev:RangeCustomEvent){
   //   this.valorRange = parseInt(ev.detail.value.toString());
   // }
 
-  calcularGorjeta(ev:RangeCustomEvent){
-    this.valorRange = Number(ev.detail.value.toString())/100;
-    this.gorjeta = Math.round(Number(this.valorRange*this.valorConta));
+  calcularGorjeta(ev: RangeCustomEvent) {
+    this.valorRange = Number(ev.detail.value.toString()) / 100;
+    this.gorjeta = (Number(this.valorRange * this.valorConta));
     this.valorTotalConta = Number(this.gorjeta + this.valorConta);
   }
 
-  converterMedidas(ev:RangeCustomEvent){
-    this.valorRange = Number(ev.detail.value.toString());
-
-    if(this.tipoMedida === "quilometros"){
-      this.convertido = Number(this.valorRange)*1000;
-    }else{
-      
+  converterMedidas(ev: RangeCustomEvent) {
+    this.valorRangeUM = Number(ev.detail.value.toString());
+    if (this.tipoMedida === "quilometros" && this.tipoMedidaConverter === "milhas") {
+      this.valorRangeKm = Number(ev.detail.value.toString()) * 1000;
+      this.convertido = (Number(this.valorRangeUM) / 1.609).toFixed(2);
+    }if(this.tipoMedida === "quilometros" && this.tipoMedidaConverter === "pes"){
+      this.valorRangeKm = Number(ev.detail.value.toString()) * 1000;
+      this.convertido = (Number(this.valorRangeUM) * 3281).toFixed(2);
+    }if(this.tipoMedida === "metros" && this.tipoMedidaConverter === "milhas"){
+      this.valorRangeM = Number(ev.detail.value.toString());
+      this.convertido = (Number(this.valorRangeUM) / 1609).toFixed(2);
+    }if(this.tipoMedida === "metros" && this.tipoMedidaConverter === "pes"){
+      this.valorRangeM = Number(ev.detail.value.toString());
+      this.convertido = (Number(this.valorRangeUM) * 3.281).toFixed(2);
     }
   }
 
 }
+
